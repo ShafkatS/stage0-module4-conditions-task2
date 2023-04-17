0b1629a69438daaaabb186ac7468783bd3f4935c
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-17 21:30:03 +0500

    Update CoinFlip.java

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -4,9 +4,9 @@ public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
         if (from1UpTo1000 >= 1 && from1UpTo1000 < 500) {
             System.out.println("Eagle");
-        } else if (from1UpTo1000 > 500 && from1UpTo1000 <= 1000) {
+        }
+        if (from1UpTo1000 >= 500 && from1UpTo1000 <= 1000){
             System.out.println("Tail");
         }
-
     }
 }
